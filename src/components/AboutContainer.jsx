cd235e262c8084f24660c638c4938716ac4be76f
Carlos Augusto Diniz Filho <ysneshy@hotmail.com>
2022-10-19 19:47:06 -0300

    1 commit

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,23 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+      Formado em Engenharia da Computação pós graduado em Engenharia de Redes e Telecomunicações pela Etacio Iesam.
+
+A minha principal ferramenta de trabalho é o React, Python e Quasar apesar de ter conhecimentos sobre PHP, C, C++ escolhi me aprofundar no front-end, especialmente no sistema do React.
+
+No momento estou consolidando conhecimento sobre Next.js e desenvolvimento com Typescript.
+      </p>
+      <p>
+      Formado em Engenharia da Computação pós graduado em Engenharia de Redes e Telecomunicações pela Etacio Iesam.
+
+A minha principal ferramenta de trabalho é o React, Python e Quasar apesar de ter conhecimentos sobre PHP, C, C++ escolhi me aprofundar no front-end, especialmente no sistema do React.
+
+No momento estou consolidando conhecimento sobre Next.js e desenvolvimento com Typescript.
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,19 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+      Formado em Engenharia da Computação pós graduado em Engenharia de Redes e Telecomunicações pela Etacio Iesam.
+
+A minha principal ferramenta de trabalho é o React, Python e Quasar apesar de ter conhecimentos sobre PHP, C, C++ escolhi me aprofundar no front-end, especialmente no sistema do React.
+
+No momento estou consolidando conhecimento sobre Next.js e desenvolvimento com Typescript.
+      </p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
